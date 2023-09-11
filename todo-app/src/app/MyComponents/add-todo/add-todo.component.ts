6eab20575cd0715cec70f7b1b55b34fb81c6008a
pavitrapoojary <poojarypavitra2@gmail.com>
2023-09-11 09:41:42 +0530

    todo app code

---
+++ todo-app/src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,26 @@
+import { Component,Output,EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent {
+  title:string | undefined;
+  desc:string | undefined;
+
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter(); 
+
+
+  onSubmit(){
+      const todo={
+        sno:8,
+        title:this.title,
+        desc:this.desc,
+        active:true
+      }
+      this.todoAdd.emit(todo);
+  }
+  
+}
